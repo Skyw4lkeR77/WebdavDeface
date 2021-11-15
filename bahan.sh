e70c5e97910450fb48fb0efebe7e49550d175fba
Skyw4lkeR77 <93298312+Skyw4lkeR77@users.noreply.github.com>
2021-11-15 22:23:26 +0700

    Create bahan.sh

---
+++ bahan.sh
@@ -0,0 +1,9 @@
+pkg install figlet
+pkg install Ruby
+pkg install lolcat
+gem install lolcat
+pkg install curl
+pip install termcolor
+clear
+figlet -f slant Suscses | lolcat
+echo "Selanjutnya Ketik sh def.sh"
